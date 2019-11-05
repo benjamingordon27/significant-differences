3fd58920a4c2524e2be27b0e65f3579e55543638
Carly Fink <carlyfink@Carlys-MacBook-Air.local>
2019-11-05 15:48:39 -0500

    try again

--- server.js
+++ server.js
@@ -204,10 +204,9 @@ app.get("/csv", function(req,res){
   res.send(output_csv_to_client(lines));
 })
 
-app.use("/input_file", function(req,res,next){    
+app.get("/input_file", function(req,res){    
   res.setHeader('Access-Control-Allow-Origin', '*');
-  next()
-},function (req, res, next) {
+  
   //Load in the input, replace spaces and quotes, and split so that we can parse the array.
   var lines = req.query.input.replace(/%20/g," ");
   lines = lines.replace(/%22/g,'"');  
