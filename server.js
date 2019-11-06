d9af604abe197a0879c53a6b86c26209a1a41939
Carly Fink <carlyfink@Carlys-MacBook-Air.local>
2019-11-06 12:57:22 -0500

    fix bug that was not allowing multiple columns to be checked properly

--- server.js
+++ server.js
@@ -2,22 +2,6 @@
 
 const fs = require('fs');
 
-//
-//
-//
-//
-//
-//
-//
-////THERE IS A PROBLEM WITH IF YOU DO 4 SIG DIFS
-////THE FOURTH COLUMN WILL NOT SHOW SIGNIFICANT DIFFERENCES PROPERLY
-////UGH
-//
-//THERE IS ALSO A PROBLEM WITH & if it's in the csv
-//
-//
-//
-
 /***
  * Function that takes a path and loads in the correct csv file.
  * Returns the data split into lines
@@ -108,7 +92,7 @@ let find_curr_letters = function(bases,letters){
 }
 
 let z_score = function(nums, bases){
-  var list_of_output = [];
+  var list_of_output = [];  
     for(var i = 0; i<nums.length;i++){
         var output =[];
         for(var j=0;j<bases.length-1;j++){          
@@ -128,42 +112,49 @@ let z_score = function(nums, bases){
             var p = (p1 * n1 + p2 * n2) / (n1 + n2);
             //var z = (p1 - p2) / (Math.sqrt((p*(1-p))*((1/n1)+(1/n2))));            
             var z = (p1 - p2) / Math.sqrt((p1 * (1 - p1) / (n1 - 1))+(p2 * (1 - p2) / (n2 - 1)));
+
+            
+
             output.push(z);
           }
         }
         //add the row of output to the current output
         list_of_output.push(output);
-      }
+      }      
       return (list_of_output);
 }
 
 //var list_of_output = z_score(nums,bases);
 
-//THE PROBLEM FOR THE FOURTCH COLUMN LIES IN THIS FUNCTION – WHY IS THE MATRIX NOT UPDATING PROPERLY
 let final_output = function(nums,bases,list_of_output){
-  //Create an empty matrix
+  console.log("columns",columns)
+
+  //Create an empty matrix  
   const matrix = new Array(nums.length).fill("").map(() => new Array(bases.length).fill(""));
-  for(var i=0;i<nums.length;i++){
-    for(var j=0;j<bases.length;j++){
-      //curr is the current z score
-      var curr = Number(list_of_output[i][j]);
-
-      //THE PROBLEM FOR THE FOURTCH COLUMN LIES IN THIS FUNCTION – WHY IS THE MATRIX NOT UPDATING PROPERLY
-
-      //if the z score is above 1.96, then there is a significant difference and we update our table
-      if(Math.abs(curr)>1.96){
-        //x is the first letter being tested, y is the second letter being tested (ex. "A C" means x is A and y is C)      
-        var x = columns[j].split(" ")[0];
-        var x_index = letters.indexOf(x);      
-        var y = columns[j].split(" ")[1];
-        var y_index = letters.indexOf(y);
-        //if positive, then update x to be significantly different than y
-        if(curr > 0){
-          matrix[i][x_index] += ""+y;
-        //if negative, then update y to be significantly different than x
-        }else{
-          matrix[i][y_index] += ""+x;
+  
+  var line_index = 0;
+  for(var i = 0; i<nums.length;i++){    
+    line_index = 0;
+    for(var j=0;j<bases.length-1;j++){          
+      for(var k=j+1;k<bases.length;k++){
+        console.log(line_index);
+        var curr = Number(list_of_output[i][line_index]);
+        console.log("j,k,curr",j,k,curr);
+        if(Math.abs(curr)>1.96){
+          //x is the first letter being tested, y is the second letter being tested (ex. "A C" means x is A and y is C)                
+          var x = columns[line_index].split(" ")[0];          
+          var x_index = letters.indexOf(x);      
+          var y = columns[line_index].split(" ")[1];
+          var y_index = letters.indexOf(y);
+          //if positive, then update x to be significantly different than y
+          if(curr > 0){
+            matrix[i][x_index] += ""+y;
+          //if negative, then update y to be significantly different than x
+          }else{
+            matrix[i][y_index] += ""+x;
+          }
         }
+        line_index++;
       }
     }
   }
